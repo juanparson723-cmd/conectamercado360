8930a788af37d90e338d35ea893d3d04bd761328
FER <juanparson723@gmail.com>
2026-06-29 00:20:47 -0600

    a

--- static/js/login.js
+++ static/js/login.js
@@ -1,293 +1,478 @@
-const abrirLogin = document.getElementById("abrirLogin");
-const cerrarLogin = document.getElementById("cerrarLogin");
+from flask import Flask #importa flask
+from flask import render_template # muestra los HTML
+from flask import request #obtiene datos enviados desde formularios
+from flask import redirect #redirigir al usuario a otra rut
+from flask import url_for #genera rutas 
+from flask import session #sesión del usuario
+from flask import flash #permite mostrar mensajes temporales al usuario
+
+from config import Config # importa la configuración de la db
+from database import get_connection # importa la función para conectarse a la base de datos
+
+import bcrypt # se utiliza para cifrar contraseñas
+
+app = Flask(__name__) # crea la aplicación Flask
+app.config.from_object(Config) #carga lo que tenga config
+
+
+# =registro
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST": #aqui se piden los datos a continuacion
+
+        nombre = request.form.get("nombre")
+        apellidos = request.form.get("apellidos")
+        correo = request.form.get("correo")
+        telefono = request.form.get("telefono")
+        password = request.form.get("password")
+        codigo = request.form.get("codigo")
+
+        conexion = get_connection() # es la conexion con la base de datos
+        cursor = conexion.cursor(dictionary=True)
+
+        #la base de datos comprueba si el correo ya esta utilizado
+        cursor.execute("""
+            SELECT * FROM usuarios
+            WHERE correo = %s
+        """, (correo,))
+
+        #si el correo ya esta registrado redirige a index
+        usuario_existente = cursor.fetchone()
+
+        if usuario_existente:
+            flash("El correo ya está registrado")
+            cursor.close()
+            conexion.close()
+            return redirect(url_for("index"))
+
+        id_r = 3 #asigna el rol en este caso usuario normal
+        id_l = None
+
+        if codigo: #este codigo relaciona al dueño con su local 
+            cursor.execute("""
+                SELECT * FROM codigos_local
+                WHERE codigo = %s
+            """, (codigo,))
+
+            #y si el codigo es valido activa su rol de administrador de su local
+            codigo_valido = cursor.fetchone()
+
+            if codigo_valido:
+                id_r = 2
+                id_l = codigo_valido["id_l"]
+
+        #aqui se encripta la contraseña gracias a bcrypt
+        password_hash = bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.gensalt()
+        ).decode("utf-8")
+
+        #aqui se insertan los datos en la base de datos 
+        cursor.execute("""
+            INSERT INTO usuarios
+            (
+                nombre,
+                apellidos,
+                correo,
+                password,
+                telefono,
+                id_r,
+                id_l
+            )
+            VALUES
+            (%s,%s,%s,%s,%s,%s,%s)
+        """, (
+            nombre,
+            apellidos,
+            correo,
+            password_hash,
+            telefono,
+            id_r,
+            id_l
+        ))
+
+        #se repite mucho por que es la representacion de que se cierra la conexion de la db
+        conexion.commit()
+
+        user_id = cursor.lastrowid
+
+        session["id_u"] = cursor.lastrowid
+        session["nombre"] = nombre
+        session["id_r"] = id_r
+        session["id_l"] = id_l
 
-const modalLogin = document.getElementById("modalLogin");
-const overlay = document.getElementById("overlay");
+        cursor.close()
+        conexion.close()
+
+        #si la cuenta se crea correctamente manda a inicio
+        flash("Cuenta creada correctamente")
+        return redirect(url_for("inicio"))
 
-const contenedor = document.getElementById("contenedorPrincipal");
+    return render_template("index.html") #se cierra la definicion de index
+
+
+# inicio de sesion
+@app.route("/iniciar_sesion", methods=["POST"])
+def iniciar_sesion():
+
+    #se pide correo y contraseña
+    correo = request.form.get("correo")
+    password = request.form.get("password")
 
-const password = document.getElementById("password");
-const confirmarPassword = document.getElementById("confirmarPassword");
+    #se repite
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
+
+    #selecciona de usuarios el correo y lo verifica
+    cursor.execute("""
+        SELECT * FROM usuarios
+        WHERE correo = %s
+    """, (correo,))
 
-const passwordMatch = document.getElementById("passwordMatch");
+    usuario = cursor.fetchone()
 
-const strengthBar = document.getElementById("strengthBar");
-const strengthText = document.getElementById("strengthText");
+    cursor.close()
+    conexion.close()
 
-const correo = document.getElementById("correo");
-const correoEstado = document.getElementById("correoEstado");
+    #si el correo es incorrecto o la contraseña lo es redirige a index
+    if not usuario:
+        flash("Correo incorrecto")
+        return redirect(url_for("index"))
 
-const codigo = document.getElementById("codigo");
-const codigoEstado = document.getElementById("codigoEstado");
+    if not bcrypt.checkpw(
+        password.encode("utf-8"),
+        usuario["password"].encode("utf-8")
+    ):
+        flash("Contraseña incorrecta")
+        return redirect(url_for("index"))
+    
+    #si todo es correcto redirige a inicio
+    session["id_u"] = usuario["id_u"]
+    session["nombre"] = usuario["nombre"]
+    session["id_r"] = usuario["id_r"]
+    session["id_l"] = usuario.get("id_l")
 
+    return redirect(url_for("inicio"))
 
 
-abrirLogin.addEventListener("click", () => {
+# el inicio XD
+@app.route("/inicio")
+def inicio():
 
-    modalLogin.classList.add("active");
+    #si no hay sesion vuelve al registro
 
-    overlay.classList.add("active");
+    if "id_u" not in session:
+        return redirect(url_for("index"))
+    
+    #se repite lo de conexion
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
 
-    contenedor.classList.add("blur");
+    #selecciona 12 productos
+    cursor.execute("""
+        SELECT *
+        FROM productos
+        LIMIT 12
+    """)
 
-});
+    productos = cursor.fetchall()
 
+    #se repite lo de conexion
+    cursor.close()
+    conexion.close()
 
+    #muestra los productos de la busqueda
+    return render_template(
+        "inicio.html",
+        productos=productos
+    )
+
+@app.route("/mi_local")
+def mi_local():
+
+    if "id_u" not in session:
+        return redirect(url_for("index"))
 
-function cerrarModal() {
+    if session["id_r"] != 2:
+        return redirect(url_for("inicio"))
 
-    modalLogin.classList.remove("active");
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
 
-    overlay.classList.remove("active");
+    cursor.execute("""
+        SELECT *
+        FROM locales
+        WHERE id_l = %s
+    """, (session.get("id_l"),))
 
-    contenedor.classList.remove("blur");
+    local = cursor.fetchone()
 
-}
+    cursor.execute("""
+        SELECT *
+        FROM productos
+        WHERE id_l = %s
+    """, (session.get("id_l"),))
 
+    productos = cursor.fetchall()
 
+    cursor.close()
+    conexion.close()
+
+    return render_template(
+        "admin_local.html",
+        local=local,
+        productos=productos
+    )
+
+
+@app.route("/panel_local")
+def panel_local():
+
+    
+    if "id_u" not in session:
+        return redirect(url_for("index"))
+
+   
+    if session["id_r"] != 2:
+        return redirect(url_for("mi_local"))
+
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT *
+        FROM productos
+        WHERE id_l = %s
+    """, (session["id_l"],))
+
+    productos = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+
+    return render_template(
+        "admin_local.html",
+        productos=productos
+    )
+
+@app.route("/agregar_producto", methods=["POST"])
+def agregar_producto():
+
+    if "id_u" not in session:
+        return redirect(url_for("index"))
 
-cerrarLogin.addEventListener("click", cerrarModal);
+    if session.get("id_r") != 2:
+        return redirect(url_for("inicio"))
+
+    nombre = request.form.get("nombre")
+    precio = request.form.get("precio")
+    imagenes = request.form.get("imagenes")
+    descripcion = request.form.get("descripcion")
+
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        INSERT INTO productos
+        (nombre, precio, imagenes, descripcion, id_l)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (
+        nombre,
+        precio,
+        imagenes,
+        descripcion,
+        session.get("id_l")
+    ))
 
-overlay.addEventListener("click", cerrarModal);
+    conexion.commit()
+    cursor.close()
+    conexion.close()
 
+    flash("Producto agregado correctamente")
+    print("id_l en sesión:", session.get("id_l"))
 
+    return redirect(url_for("mi_local"))
 
-function togglePassword(id) {
-
-    const campo = document.getElementById(id);
-
-    if (campo.type === "password") {
-
-        campo.type = "text";
-
-    } else {
-
-        campo.type = "password";
-
-    }
-
-}
-
-
-
-confirmarPassword.addEventListener("input", () => {
-
-    if (confirmarPassword.value === "") {
-
-        passwordMatch.textContent = "";
-
-        return;
-
-    }
-
-    if (password.value === confirmarPassword.value) {
-
-        passwordMatch.textContent =
-            "Las contraseñas coinciden";
-
-        passwordMatch.style.color = "#3DDC84";
-
-    } else {
-
-        passwordMatch.textContent =
-            "Las contraseñas no coinciden";
-
-        passwordMatch.style.color = "#FF5E5E";
-
-    }
-
-});
-
-
-
-password.addEventListener("input", () => {
-
-    const valor = password.value;
-
-    let fuerza = 0;
-
-    if (valor.length >= 8) {
-
-        fuerza++;
-
-    }
-
-    if (/[A-Z]/.test(valor)) {
-
-        fuerza++;
-
-    }
-
-    if (/[0-9]/.test(valor)) {
-
-        fuerza++;
-
-    }
-
-    if (/[^A-Za-z0-9]/.test(valor)) {
-
-        fuerza++;
-
-    }
-
-    switch (fuerza) {
-
-        case 0:
-
-            strengthBar.style.width = "0%";
-
-            strengthText.textContent =
-                "Seguridad de contraseña";
-
-            break;
-
-        case 1:
-
-            strengthBar.style.width = "25%";
-
-            strengthBar.style.background =
-                "#FF5E5E";
-
-            strengthText.textContent =
-                "Contraseña débil";
-
-            break;
-
-        case 2:
-
-            strengthBar.style.width = "50%";
-
-            strengthBar.style.background =
-                "#FFB347";
-
-            strengthText.textContent =
-                "Contraseña media";
-
-            break;
-
-        case 3:
-
-            strengthBar.style.width = "75%";
-
-            strengthBar.style.background =
-                "#FFD700";
-
-            strengthText.textContent =
-                "Contraseña buena";
-
-            break;
-
-        case 4:
-
-            strengthBar.style.width = "100%";
-
-            strengthBar.style.background =
-                "#3DDC84";
-
-            strengthText.textContent =
-                "Contraseña fuerte";
-
-            break;
-
-    }
-
-});
-
-
-
-correo.addEventListener("input", () => {
-
-    const regex =
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (correo.value === "") {
-
-        correoEstado.textContent = "";
-
-        return;
-
-    }
-
-    if (regex.test(correo.value)) {
-
-        correoEstado.textContent =
-            "Correo válido";
-
-        correoEstado.style.color =
-            "#3DDC84";
-
-    } else {
-
-        correoEstado.textContent =
-            "Correo inválido";
-
-        correoEstado.style.color =
-            "#FF5E5E";
-
-    }
-
-});
-
-
-
-codigo.addEventListener("input", () => {
-
-    if (codigo.value.trim() === "") {
-
-        codigoEstado.textContent = "";
-
-        return;
-
-    }
-
-    if (codigo.value.length >= 5) {
-
-        codigoEstado.textContent =
-            "Código listo para validar";
-
-        codigoEstado.style.color =
-            "#D4AF37";
-
-    }
-
-});
-
-
-
-document.addEventListener("keydown", (e) => {
-
-    if (
-        e.key === "Escape" &&
-        modalLogin.classList.contains("active")
-    ) {
-
-        cerrarModal();
-
-    }
-
-});
-
-
-
-document
-.getElementById("registroForm")
-.addEventListener("submit", (e) => {
-
-    if (
-        password.value !==
-        confirmarPassword.value
-    ) {
-
-        e.preventDefault();
-
-        passwordMatch.textContent =
-            "Las contraseñas no coinciden";
-
-        passwordMatch.style.color =
-            "#FF5E5E";
-
-        return;
-
-    }
-
-});
+@app.route("/editar_producto/<int:id_p>", methods=["POST"])
+def editar_producto(id_p):
+
+    if "id_u" not in session:
+        return redirect(url_for("index"))
+
+    if session.get("id_r") != 2:
+        return redirect(url_for("inicio"))
+
+    nombre = request.form.get("nombre")
+    precio = request.form.get("precio")
+    imagenes = request.form.get("imagenes")
+    descripcion = request.form.get("descripcion")
+
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        UPDATE productos
+        SET
+            nombre = %s,
+            precio = %s,
+            imagenes = %s,
+            descripcion = %s
+        WHERE
+            id_p = %s
+            AND id_l = %s
+    """, (
+        nombre,
+        precio,
+        imagenes,
+        descripcion,
+        id_p,
+        session.get("id_l")
+    ))
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    flash("Producto actualizado correctamente")
+
+    return redirect(url_for("mi_local"))
+
+# salida
+@app.route("/logout")
+def logout():
+
+    #redirije a la plantilla principal
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/eliminar_producto/<int:id_p>", methods=["POST"])
+def eliminar_producto(id_p):
+
+    if "id_u" not in session:
+        return redirect(url_for("index"))
+
+    if session.get("id_r") != 2:
+        return redirect(url_for("inicio"))
+
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        DELETE FROM productos
+        WHERE
+            id_p=%s
+            AND id_l=%s
+    """, (
+        id_p,
+        session.get("id_l")
+    ))
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    flash("Producto eliminado correctamente")
+
+    return redirect(url_for("mi_local"))
+
+# =busqueda
+@app.route("/resultados")
+def resultados():
+
+    #el parametro de la busqueda en este caso q
+    busqueda = request.args.get("q", "")
+
+    #se repite la conexion
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
+
+    #resultados que debe dar de la busqueda o de lo seleccionado
+    sql = """
+    SELECT
+        p.id_p,
+        p.nombre AS producto,
+        p.precio,
+        p.imagenes,
+        l.id_l,
+        l.nombre AS local
+    FROM productos p
+    INNER JOIN locales l ON p.id_l = l.id_l
+    WHERE p.nombre LIKE %s
+       OR l.nombre LIKE %s
+    """
+
+    #la conexion ejecuta la busqueda
+    cursor.execute(sql, (
+        f"%{busqueda}%",
+        f"%{busqueda}%"
+    ))
+
+    #muestra los resultados 
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+
+    #redirige a los resultados y la busqueda
+    return render_template(
+        "resultados.html",
+        resultados=resultados,
+        busqueda=busqueda
+    )
+
+#busqueda de locales por id
+@app.route("/local/<int:id_l>")
+def local(id_l):
+
+    #se repite
+    conexion = get_connection()
+    cursor = conexion.cursor(dictionary=True)
+
+    #ejecucion de la busqueda de locales por id
+    cursor.execute("""
+        SELECT * FROM locales
+        WHERE id_l = %s
+    """, (id_l,))
+
+    local = cursor.fetchone()
+
+    #ejecucion de la busqueda por  id
+    cursor.execute("""
+        SELECT * FROM productos
+        WHERE id_l = %s
+    """, (id_l,))
+
+    productos = cursor.fetchall()
+
+    #se repite
+    cursor.close()
+    conexion.close()
+
+    #muestra local y productos
+    return render_template(
+        "local.html",
+        local=local,
+        productos=productos
+    )
+
+@app.route("/admin") #definicion del panel de admin
+def admin():
+
+    #lo redirige a index si no tiene secion
+    if "id_u" not in session:
+        return redirect(url_for("index"))
+    
+    #si el id es igual a dos te da acceso al panel de admin
+    if session["id_r"] != 1:
+        return redirect(url_for("inicio"))
+
+    return render_template("admin.html")
+
+
+#ejecucion de la app 
+if __name__ == "__main__":
+    app.run(debug=True)
